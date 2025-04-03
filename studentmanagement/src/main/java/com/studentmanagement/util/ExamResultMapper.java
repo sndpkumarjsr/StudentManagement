9bc70e5b469fc3dece51f31dfab88f3145a89051
Sandeep <sndpkumarjsr@hotmail.com>
2025-04-04 00:12:54 +0530

    Grade Enhancing

--- studentmanagement/src/main/java/com/studentmanagement/util/ExamResultMapper.java
+++ studentmanagement/src/main/java/com/studentmanagement/util/ExamResultMapper.java
@@ -1,12 +1,7 @@
 package com.studentmanagement.util;
 
-import com.studentmanagement.dto.ExamResultDto;
 import com.studentmanagement.dto.ExamResultResponseDto;
-import com.studentmanagement.entity.Exam;
 import com.studentmanagement.entity.ExamResult;
-import com.studentmanagement.entity.Student;
-import com.studentmanagement.repository.ExamRepository;
-import com.studentmanagement.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
